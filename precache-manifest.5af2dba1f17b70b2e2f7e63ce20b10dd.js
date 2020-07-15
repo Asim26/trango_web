4840b3c2a48b812788b513ed52b1dc665955b378
Asim26 <asimmehmood807@gmail.com>
2020-07-15 23:24:59 +0500

    Updates

---
+++ precache-manifest.5af2dba1f17b70b2e2f7e63ce20b10dd.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc04432dbe80a175777ef58e8b1aa2d",
+    "url": "/trango_web/index.html"
+  },
+  {
+    "revision": "d3a7dd7e7814240ffe87",
+    "url": "/trango_web/static/css/2.91c29527.chunk.css"
+  },
+  {
+    "revision": "e408d82a66af730f84c7",
+    "url": "/trango_web/static/css/main.35eaceb4.chunk.css"
+  },
+  {
+    "revision": "d3a7dd7e7814240ffe87",
+    "url": "/trango_web/static/js/2.cf3efff6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/trango_web/static/js/2.cf3efff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e408d82a66af730f84c7",
+    "url": "/trango_web/static/js/main.662783e6.chunk.js"
+  },
+  {
+    "revision": "29ca1a3d9c490d6bf97b",
+    "url": "/trango_web/static/js/runtime-main.89c60417.js"
+  },
+  {
+    "revision": "1874fd81f8957d865a495a201d0eaf23",
+    "url": "/trango_web/static/media/ImageFooter.1874fd81.png"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/trango_web/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "d1b73f4217fa8714381ce9837ac1eb21",
+    "url": "/trango_web/static/media/SelfHosted.d1b73f42.png"
+  },
+  {
+    "revision": "c1c585a0da37e7ed974cfe6e188e1964",
+    "url": "/trango_web/static/media/aboveFold.c1c585a0.png"
+  },
+  {
+    "revision": "4bdc1bec73da9a5957df67a8330a6deb",
+    "url": "/trango_web/static/media/appstore@2x.4bdc1bec.png"
+  },
+  {
+    "revision": "04294bdc3af037f6a58faa5848f7ab61",
+    "url": "/trango_web/static/media/appstore@3x.04294bdc.png"
+  },
+  {
+    "revision": "1331d29ff1442396418a2899a38970b1",
+    "url": "/trango_web/static/media/audiocall.1331d29f.svg"
+  },
+  {
+    "revision": "97c1153aabb90db35475435b95709a66",
+    "url": "/trango_web/static/media/background.97c1153a.svg"
+  },
+  {
+    "revision": "27e122dbfe9ed219fded48435b0458ec",
+    "url": "/trango_web/static/media/block1.27e122db.png"
+  },
+  {
+    "revision": "229338ebf1db986c1c31256fe0fa100d",
+    "url": "/trango_web/static/media/block2.229338eb.png"
+  },
+  {
+    "revision": "8d3fa4f58299f01fda555b47abe1df4a",
+    "url": "/trango_web/static/media/block3.8d3fa4f5.png"
+  },
+  {
+    "revision": "32082b7a59c9750e759c71f65c7f0e9c",
+    "url": "/trango_web/static/media/business.32082b7a.svg"
+  },
+  {
+    "revision": "25695015cbb96ae35cc575aca3608410",
+    "url": "/trango_web/static/media/chat.25695015.svg"
+  },
+  {
+    "revision": "c644c05dfabd3568395589487d2ae1e7",
+    "url": "/trango_web/static/media/cloud-or-host.c644c05d.svg"
+  },
+  {
+    "revision": "32082b7a59c9750e759c71f65c7f0e9c",
+    "url": "/trango_web/static/media/consumer.32082b7a.svg"
+  },
+  {
+    "revision": "3ecfec57443100d33f6772048c53ddf8",
+    "url": "/trango_web/static/media/consumer@2x.3ecfec57.png"
+  },
+  {
+    "revision": "0c56e45b872665f50a662ad3a6eccec0",
+    "url": "/trango_web/static/media/consumerApp.0c56e45b.png"
+  },
+  {
+    "revision": "10b93d8183f6d31c9a4bcd042c3f2ddc",
+    "url": "/trango_web/static/media/contactUs.10b93d81.png"
+  },
+  {
+    "revision": "af2dfd726ed01af133fcaf3e4707ebdf",
+    "url": "/trango_web/static/media/cross.af2dfd72.png"
+  },
+  {
+    "revision": "73a05fb6da951498e670eabdb163c5d1",
+    "url": "/trango_web/static/media/email-logo.73a05fb6.svg"
+  },
+  {
+    "revision": "49d567b2ebbca44203fdeb3936058e7d",
+    "url": "/trango_web/static/media/file-sharing.49d567b2.svg"
+  },
+  {
+    "revision": "cd9b54e2fa0cea38838b864ce5291ce4",
+    "url": "/trango_web/static/media/googleplay@2x.cd9b54e2.png"
+  },
+  {
+    "revision": "e2a09adf21c02d5ee84e76127fa889ce",
+    "url": "/trango_web/static/media/googleplay@3x.e2a09adf.png"
+  },
+  {
+    "revision": "a840f963f73ee29d23cbb809e940fbe5",
+    "url": "/trango_web/static/media/how.a840f963.png"
+  },
+  {
+    "revision": "da3485f72d4fbfc76b9ed992f13e5e17",
+    "url": "/trango_web/static/media/location-logo.da3485f7.svg"
+  },
+  {
+    "revision": "77b6a2ee00e06aff1e6827e04bc59173",
+    "url": "/trango_web/static/media/logoFooter.77b6a2ee.png"
+  },
+  {
+    "revision": "ca6a3ee4d25fe33d83c1a6b4f33266a3",
+    "url": "/trango_web/static/media/mob1.ca6a3ee4.png"
+  },
+  {
+    "revision": "c2c1adc58b4fb157a7d10bb55a6cff87",
+    "url": "/trango_web/static/media/mob2.c2c1adc5.png"
+  },
+  {
+    "revision": "377adf16cc7701d675fb359cd2cd6a6c",
+    "url": "/trango_web/static/media/mob3.377adf16.png"
+  },
+  {
+    "revision": "73c1b9e9ead080bb3976bce9c4bb3d50",
+    "url": "/trango_web/static/media/mob4.73c1b9e9.png"
+  },
+  {
+    "revision": "c3da6d275a3f697b214db4663faad98b",
+    "url": "/trango_web/static/media/mob5.c3da6d27.png"
+  },
+  {
+    "revision": "a1e1dd92699499d3f31cff9f7c726d6b",
+    "url": "/trango_web/static/media/mob6.a1e1dd92.png"
+  },
+  {
+    "revision": "45d7c0749078a116e4a880c2c2fbc9ed",
+    "url": "/trango_web/static/media/mob7.45d7c074.png"
+  },
+  {
+    "revision": "916fb6196d86d4031d790400ce7ee4ad",
+    "url": "/trango_web/static/media/mob8.916fb619.png"
+  },
+  {
+    "revision": "66c30a48e5bbfc0a57c36d1bff6eb00b",
+    "url": "/trango_web/static/media/mobile.66c30a48.png"
+  },
+  {
+    "revision": "324c5d309f3edd5a194c8e498e5b50d0",
+    "url": "/trango_web/static/media/no-internet-problem.324c5d30.svg"
+  },
+  {
+    "revision": "88829979ca44935298c5ca6d964655f7",
+    "url": "/trango_web/static/media/pc.88829979.png"
+  },
+  {
+    "revision": "9bf0651ec45cd11e4f6c00a3d634549b",
+    "url": "/trango_web/static/media/pcContent.9bf0651e.png"
+  },
+  {
+    "revision": "35dd3792ffc3be31a08ac294eece7505",
+    "url": "/trango_web/static/media/phone-logo.35dd3792.svg"
+  },
+  {
+    "revision": "064f1716fdb66e0e61e7b0e330f46959",
+    "url": "/trango_web/static/media/privacy-first.064f1716.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/trango_web/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/trango_web/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/trango_web/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "dcb89611a6c027991569ff4db01f4dea",
+    "url": "/trango_web/static/media/scanScreen.dcb89611.png"
+  },
+  {
+    "revision": "bee8a93158a4054130a665e735f47b6c",
+    "url": "/trango_web/static/media/self-cloud-hosted.bee8a931.svg"
+  },
+  {
+    "revision": "ed9be4114727cfc0d546c03c0a05b726",
+    "url": "/trango_web/static/media/self-hosted.ed9be411.svg"
+  },
+  {
+    "revision": "eebd5a346d72fd8b21fbbc058c3f81eb",
+    "url": "/trango_web/static/media/selfCloudHosted.eebd5a34.png"
+  },
+  {
+    "revision": "eda8c9a5c60eb08c92b2e355e1c0e42e",
+    "url": "/trango_web/static/media/tick.eda8c9a5.png"
+  },
+  {
+    "revision": "835cb2cf8e4c838430befbc398151300",
+    "url": "/trango_web/static/media/video-calling.835cb2cf.svg"
+  }
+]);
